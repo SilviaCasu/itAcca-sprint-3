db83c6e6f46d2701dafe4381b6ed74f8a29d59b7
Silvia <sil.casu@gmail.com>
2022-07-22 09:47:33 +0200

    commit inicial de shop

--- js/checkout.js
+++ js/checkout.js
@@ -2,27 +2,97 @@
 // Exercise 6
 function validate() {
 	var error = 0;
+
+
 	// Get the input fields
+	var form = document.querySelectorAll('.needs-validation');
+
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
-	// Get the error elements
+	// Get the error elements""
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	var errorEmail = document.getElementById("errorEmail");
+	var errorAddress = document.getElementById("errorAddress");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorPhone = document.getElementById("errorPhone");
+
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
+
+
+	let regulExName = new RegExp(/^[A-Za-z]+$/);
+
+	if (fName.value.length >= 3 && regulExName.test(fName.value)) {
+	} else {
+		document.getElementById("errorName").style.display = 'block';
+		document.getElementById("fName").style.border = '2px solid red';
+	
+		error = 1;
 	}
 
-	if(fEmail.value == ""){
-		error++;
+	let regulExLastN = new RegExp(/^[A-Za-z]+$/);
+
+	if (fLastN.value.length >= 3 && regulExLastN.test(fLastN.value)) {
+	} else {
+		document.getElementById("errorLastN").style.display = 'block';
+		document.getElementById("fLastN").style.border = '2px solid red';
+		error = 1;
 	}
-	 
-	if(error>0){
+
+
+	let regulExEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+
+	if (fEmail.value.length >= 3 && regulExEmail.test(fEmail.value)) {
+
+	} else {
+		document.getElementById("errorEmail").style.display = 'block';
+		document.getElementById("fEmail").style.border = '2px solid red';
+		error = 1;
+	}
+
+
+	let regulExFPhone = new RegExp(/^[0-9]+$/);
+
+	if (fPhone.value.length === 9 && regulExFPhone.test(fPhone.value)) {
+
+	} else {
+		document.getElementById("errorPhone").style.display = 'block';
+		document.getElementById("fPhone").style.border = '2px solid red';
+		error = 1;
+	}
+
+	let regulExPass = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{4,8}$/);
+
+	if ( regulExPass.test(fPassword.value)) {
+
+	} else {
+		document.getElementById("errorPassword").style.display = 'block';
+		document.getElementById("fPassword").style.border = '2px solid red';
+		error = 1;
+	}
+
+	let regulExAdress = new RegExp(/[A-Za-z0-9]+/g);
+
+	if (fAddress.value.length >= 3 && regulExAdress.test(fAddress.value)) {
+	} else {
+		document.getElementById("errorAddress").style.display = 'block';
+		document.getElementById("fAddress").style.border = '2px solid red';
+
+		error = 1;
+	}
+
+	if (error > 0) {
 		alert("Error");
-	}else{
-		alert("OK");
+	} else {
+		alert("ok");
+		form.submit();
 	}
+
 
 }
+
--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,6 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
-   {
+    {
         id: 1,
         name: 'cooking oil',
         price: 10.5,
@@ -72,54 +72,169 @@ var cart = [];
 var total = 0;
 
 // Exercise 1
-function buy(id) {
+//comento funcion buy para emplementar addToCard;
+//-------------------------------------------------
+/*function buy(id) { 
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
+    const PRODUCT_LONG = products.length;
+    for (let i = 0; i < PRODUCT_LONG; i++) {
+        if (products[i].id === id) {
+            // 2. Add found product to the cartList array
+            cartList.push(products[i]);
+            console.log(cartList);
+            generateCart();
+        }
+    }
+    
+} //-----------------------------------------------
+*/
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.length = 0;
+    cart.length = 0;
+    document.getElementById('count_product').innerHTML = 0;
 }
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
+    var total = 0;
+    for (let item of cartList) {
+        console.log(item)
+        total += item.price;
+        console.log(total);
+        // Calculate total price of the cart using the "cartList" array
+    }
+
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+// comento funcion para emplementar addToCard;
+//--------------------------------------------------------
+/*function generateCart() {
+    cart = [];
+    for (var key in cartList) {
+        let idProduct = cartList[key].id;
+        let productEx = cart.find(e => e.id === idProduct);//busco id producto y comparo
+        // now search id in cart
+        if (productEx) {
+            // añado +1 a quantity 
+            productEx.quantity += 1;
+            productEx.subtotal = parseInt(productEx.price * productEx.quantity);
+            productEx.subtotalWithDiscount = applyPromotionsCart(productEx.id, productEx.quantity, productEx.subtotal);
+            // alert(productEx.quantity);
+            //alert(productEx.subtotal);
+        } else {
+            // añado el item al array cart con quantity=1
+            cart.push(cartList[key]);
+            cart[(cart.length) - 1].quantity = 1;
+            cart[(cart.length) - 1].subtotal = cart[(cart.length) - 1].price;
+            console.log(cart);
+        }
+    }
+}*/
+//--------------------------------------------------
 
 // Exercise 5
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
-}
-
-// Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+    var cartPrint = document.getElementById('cart_list');
+    while (cartPrint.firstChild) {
+        cartPrint.removeChild(cartPrint.lastChild);
+    }
+    let total = 0;
+    cart.forEach((product, index) => {
+        var rowProduct = cartPrint.insertRow(index);
+        rowProduct.insertCell(0).innerHTML = product.name;
+        rowProduct.insertCell(1).innerHTML = "$" + product.price;
+        rowProduct.insertCell(2).innerHTML = product.quantity;
+        let sub = 0;
+        if (product.subtotalWithDiscount) {
+            sub = product.subtotalWithDiscount;
+        } else {
+            sub = product.subtotal;
+        }
+        rowProduct.insertCell(3).innerHTML = "$" + sub.toFixed(2);
+        (total += sub).toFixed(2);
+
+        rowProduct.insertCell(4).innerHTML = '<a href="javascript:void(0)" class="btn btn-primary m-3 btn-sm" onclick="removeFromCart(' + product.id + ',' + product.price + ',' + product.quantity + ')" type="button" value="rem" />rem</a>';
+
+    }
+
+    ); quantitySw();
+
+    document.getElementById('total_price').innerHTML = total.toFixed(2);
 }
 
+//------------------------------------------------------------------------------------
 
 // ** Nivell II **
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    // Refactor previous code in order to simplify it
+    const PRODUCT_LONG = products.length;// verifico se il ID existe
+    for (let i = 0; i < PRODUCT_LONG; i++) {
+        if (products[i].id === id) {
+            let productEx = cart.find(e => e.id === products[i].id); //busco ID y comparo
+            if (productEx) {      // si ya está en cart añado qty y controlo promo
+                productEx.quantity += 1; 
+                productEx.subtotal = (productEx.price * productEx.quantity);
+                productEx.subtotalWithDiscount = applyPromotionsCart(productEx.id, productEx.quantity, productEx.subtotal);
+             
+            } else {
+                cart.push(products[i]); //si no está en cart añado producto 
+                cart[(cart.length) - 1].quantity = 1;
+                cart[(cart.length) - 1].subtotal = cart[(cart.length) - 1].price;
+                console.log(products[i]);
+            }
+        }
+    }
+    quantitySw();
+
 }
+
+
+function quantitySw() { //fun para contar productos en el cart;
+    let quantity = 0;
+    for (let i = 0; i < cart.length; i++) {
+        quantity += cart[i].quantity;
+    }
+    document.getElementById('count_product').innerHTML = quantity;
+}
+
+function applyPromotionsCart(idProduct, quantity, subtotal) {
+    if (idProduct === 1 && quantity >= 3) {
+        return (10 * quantity);
+    }
+    if
+        (idProduct === 3 && quantity >= 10)
+        return (subtotal / 3 * 2);
+}
+
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+    for (let i = 0; i < cart.length; i++) { // control ID y si qty es === 1 resto 1;
+        if (cart[i].id === id) {
+            if (cart[i].quantity === 1) {
+                cart.splice(i, 1) 
+                return printCart();
+            }
+            if (cart[i].quantity >= 1) { // control ID y si qty es >= 1 resto 1 y hago control promo;
+                cart[i].quantity = cart[i].quantity - 1;
+                cart[i].subtotal = (cart[i].price * (cart[i].quantity));
+                cart[i].subtotalWithDiscount = applyPromotionsCart(cart[i].id, cart[i].quantity, cart[i].subtotal);
+                printCart();
+            }
+        }
+    }
+    quantitySw();
+}
+console.log(cart);
+
+
+function open_modal() {
+    console.log("Open Modal");
+    printCart();
 }
